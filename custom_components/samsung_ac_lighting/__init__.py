e3d957c9b61c4e1611cf2089037a7cdc8379144e
Renato Elias <renatolelias@hotmail.com>
2026-01-23 13:05:15 -0300

    Fix config entry setup for new Home Assistant API
    - Replace deprecated async_forward_entry_setup
    - Use async_forward_entry_setups with platform list

--- custom_components/samsung_ac_lighting/__init__.py
+++ custom_components/samsung_ac_lighting/__init__.py
@@ -3,18 +3,17 @@ from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
+PLATFORMS = ["switch"]
+
 
 async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "switch")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    return await hass.config_entries.async_forward_entry_unload(entry, "switch")
-
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
